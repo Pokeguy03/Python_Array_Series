573a1325928b8da375e559b1b5113e85a7c7af6d
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2025-12-13 22:14:44 +0530

    Update and rename Min_heap for finding any n largest element to Min_heap for finding any n largest element.py

---
+++ Min_heap for finding any n largest element.py
@@ -0,0 +1,14 @@
+import heapq
+
+def kth_largest(arr, k):
+    if len(arr) < k:
+        return -1
+
+    heap = []
+
+    for num in arr:
+        heapq.heappush(heap, num)
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    return heap[0]
