ed06d12ed96e0e4bb91b0bf0c469008b8419d2e3
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2025-12-16 12:04:07 +0530

    Create Reverse_an_array.py

---
+++ Reverse_an_array.py
@@ -0,0 +1,20 @@
+#method 1
+ar=[1,2,3,4]
+ar.reverse()
+print(ar)
+
+# #method 2
+ar = [1, 2, 3, 4]
+reverse = ar[::-1]
+print(reverse)
+
+#method 3
+ar=[1,2,3,4]
+left=0
+right=len(ar)-1
+while left < right:
+    ar[left],ar[right]=ar[right],ar[left]
+    left+=1
+    right-=1
+print(ar)
+
