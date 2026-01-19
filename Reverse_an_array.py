44b04e6debe2694fb8bd11675192453e0a766379
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2026-01-19 20:42:20 +0530

    Update Reverse_an_array.py

--- Reverse_an_array.py
+++ Reverse_an_array.py
@@ -18,3 +18,12 @@ while left < right:
     right-=1
 print(ar)
 
+#For only integer:
+class Solution:
+	def reverseDigits(self, n):
+	    rev=0
+	    while n>0:
+	        digit=n%10
+	        rev=rev*10+digit
+	        n//=10
+	    return rev
